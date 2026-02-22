e85682c94320b73dbe30e23fdc308bd0a669b25c
nehalskdev <nikesk1999@gmail.com>
2026-02-22 15:19:41 +0530

    📌chore: add package.json for project setup with dependencies and scripts

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # namaste react 🚀
+
+# parcel
+
+-dev build
+-local server
+-HMR = Hot Module Replacement
+-file watching algorithem - written in c++
+
+- faster builds because CACHING
+- image optimization
+- minification
+- bundling
+  -compressing
+  -consist hashing (google it)
+- error handling
--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a H1 tag"),
-    React.createElement("h1", {}, "I am a H1 tag"),
-  ]),
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
