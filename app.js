f6d63c40f783114b083174f2db3483789362b9dc
nehalskdev <nikesk1999@gmail.com>
2026-02-22 11:44:14 +0530

    Add initial React application structure with HTML, CSS, and JavaScript files

---
+++ app.js
@@ -0,0 +1,12 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a H1 tag"),
+    React.createElement("h1", {}, "I am a H1 tag"),
+  ]),
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
